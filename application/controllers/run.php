30b026c158ed1396fbe4cd355382a9f0c14d6454
ehime <dodomeki@gmail.com>
2014-05-31 15:20:25 -0600

    Basic CI installation

---
+++ application/controllers/run.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Run extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('run_message');
+    }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/run_message.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+
+
+    <link src="/static/css/base.css" />
+
+</head>
+<body>
+
+<div id="container">
+    <h1>Welcome to CodeIgniter!</h1>
+
+    <div id="body">
+        hit
+    </div>
+
+    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
